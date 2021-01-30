e42417173c2dba5de4d860b2125abdc7c1cdc64d
Allison Levine <allison.s.levine@gmail.com>
2021-01-30 17:41:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,20 +16,7 @@ function logWhisper(string) {
     console.log (string.toLowerCase());
   }
 
-  function sayHiToGrandma(string) {
-    return string
-    if (string ===string.toLowerCase) {
-        return "I can\'t hear you!"
-    }
-  }
 
-  function sayHiToGrandma(string) {
-    var lowercase = string.toLowerCase;
-    var uppercase = string.toUpperCase;
-    if (string === lowercase) {
-      return "I can\'t hear you!"
-    }
-  }
 
   function sayHiToGrandma(string) {
     var lowercase = string.toLowerCase;

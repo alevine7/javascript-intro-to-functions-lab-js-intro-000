12cddf481b61ddec4fd1e310eeee4956b34a8796
Allison Levine <allison.s.levine@gmail.com>
2021-01-30 17:39:13 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,43 @@
+function shout(str) {
+    return str.toUpperCase() // we'll get to this in a sec
+  }
+
+
+function whisper(string) {
+    return string.toLowerCase() // we'll get to this in a sec
+  }
+
+
+function logShout(string) {
+    console.log (string.toUpperCase());
+  }
+
+function logWhisper(string) {
+    console.log (string.toLowerCase());
+  }
+
+  function sayHiToGrandma(string) {
+    return string
+    if (string ===string.toLowerCase) {
+        return "I can\'t hear you!"
+    }
+  }
+  
+  function sayHiToGrandma(string) {
+    var lowercase = string.toLowerCase;
+    var uppercase = string.toUpperCase;
+    if (string === lowercase) {
+      return "I can\'t hear you!"
+    }
+  }
+
+  function sayHiToGrandma(string) {
+    var lowercase = string.toLowerCase;
+    var uppercase = string.toUpperCase;
+    if (return string.toLowerCase()) {
+      return "I can\'t hear you!"
+    }
+    if (return string.toUpperCase()) {
+      return "YES INDEED!"
+    }
+  }
